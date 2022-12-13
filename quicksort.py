6718592bc8e82fa1b9e5f72fc82edb62bba75c7f
Leonardo Leites <leoleites2002@gmail.com>
2022-12-13 10:50:17 -0300

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,27 @@
+#import time
+
+vet = [1,0,34,12,7,8,525]
+
+#start = time.clock()
+
+def quick(vet):
+  if len(vet) <=1:
+    return vet
+  pivo = vet[:1]
+  vet = vet[1:]
+
+  lista1 = []
+  lista2 = []
+  for item in vet:
+    if item < pivo[0]:
+      lista1.append(item)
+      # print(lista1)
+    else:
+      lista2.append(item)
+      # print(lista2)
+  return (quick(lista1) + pivo + quick(lista2))
+
+# end = time.clock() 
+
+print(quick(vet))
+#print(end - start)
